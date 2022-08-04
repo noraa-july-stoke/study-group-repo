b0608272bb12ab28395d40a9bb246cf446dad700
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-08-04 18:36:22 -0400

    prompt added

---
+++ matrix-reverse-prompt.js
@@ -0,0 +1,20 @@
+//Write a function that takes in an array and returns it with the rows
+
+
+
+//matrix = [[5, 9, 21],
+//           [9, 19, 6],
+//           [12, 14, 15]]
+
+//matrixReverse(matrix) => [ [ 5, 9, 12 ],
+//                           [ 9, 19, 14 ],
+//                           [ 21, 6, 15 ] ]
+
+//matrix = [[5, 10, 8, 6],
+//           [10, 2, 7, 9],
+//           [21, 15, 19, 10]]
+
+//matrixReverse(matrix) => [ [ 5, 10, 21 ],
+//                           [ 10, 2, 15 ],
+//                           [ 8, 7, 19 ],
+//                           [ 6, 9, 10 ] ]
