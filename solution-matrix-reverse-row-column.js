547a040d9f1bf7b4eff9d5456f3d3936ba13d1df
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-08-04 18:28:59 -0400

    rename folder

--- solution-matrix-reverse-row-column.js
+++ solution-matrix-reverse-row-column.js
@@ -1,13 +1,6 @@
 let matrixReverse = function(matrix) {
     //The following are to be used to turn this into the lucky
     let columns = []
-    let maxList = []
-    let minList = []
-    let max = 0
-    let min = 0
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//****************************************************************
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     let row = 0
     let rowItem = 0
     while (rowItem < matrix[row].length) {
@@ -20,21 +13,18 @@ let matrixReverse = function(matrix) {
         row = 0
         rowItem++
     }
-
     return columns
 }
-
-
 matrix = [[5, 9, 21],
 [9, 19, 6],
 [12, 14, 15]]
 
-console.log(matrixReverse(matrix)); // [12]
+console.log(matrixReverse(matrix))
 
 matrix = [[5, 10, 8, 6],
 [10, 2, 7, 9],
 [21, 15, 19, 10]]
 
-console.log(matrixReverse(matrix)); // [10]
+console.log(matrixReverse(matrix))
 
 //functionality complete
