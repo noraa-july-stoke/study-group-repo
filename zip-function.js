fe107fb03d062978f7c0f3acb3249ec346b288eb
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-08-05 16:26:33 -0400

    add zip

---
+++ zip-function.js
@@ -0,0 +1,29 @@
+//****************PROMPT******PROMPT*******************PROMPT****************
+//********PROMPT***********PROMPT*********PROMPT*****************************
+/*Write a function zip that accepts two arrays as arguments.
+The function should return a two dimensional array with
+subarrays of length 2 that contain elements at corresponding
+indices from the input arrays.You may assume that the input
+arrays have the same length.*/
+
+//***************************************************************************
+//***************************************************************************
+
+let zip = (arr1, arr2) => {
+    let zipped = []
+    for (let i = 0; i < arr1.length; i++) {
+        zipped.push([arr1[i], arr2[i]])
+    }
+    return zipped
+
+}
+
+
+console.log(zip([1, 2, 3, 4], ['eins', 'zwei', 'drei', 'vier']));
+// [ [ 1, 'eins' ], [ 2, 'zwei' ], [ 3, 'drei' ], [ 4, 'vier' ] ]
+
+console.log(zip(['eins', 'zwei', 'drei'], [1, 2, 3]));
+// [ [ 'eins', 1 ], [ 'zwei', 2 ], [ 'drei', 3 ] ]
+
+console.log(zip(['alef', 'bet'], ['alpha', 'beta']));
+// [ [ 'alef', 'alpha' ], [ 'bet', 'beta' ] ]
