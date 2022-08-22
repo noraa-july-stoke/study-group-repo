a6acced4e15a3502c8849d5be6470bc9d63a5d41
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-08-22 10:51:30 -0400

    03

--- recursion/03week-extra-practice/practice-for-week-03-additional-practice/problems/03-even-number-range.js
+++ recursion/03week-extra-practice/practice-for-week-03-additional-practice/problems/03-even-number-range.js
@@ -1,6 +1,6 @@
 /* Even Number Range
 
-    Write a function that accepts two numbers. 
+    Write a function that accepts two numbers.
     The function should return an array of all even numbers between the two arguments inclusively.
 
     console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
@@ -8,9 +8,14 @@
     console.log(evenNumberRange(2, 0)); // prints []
 */
 
-function evenNumberRange(num1, num2) {
-    // Your code here
+function evenNumberRange(num1, num2, array = []) {
+    if (num1===num2) return array.reverse();
+    array.push(num2);
+    num2-=1;
+    return evenNumberRange(num1, num2, array);
 }
+
+console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
